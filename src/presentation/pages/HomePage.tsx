546a3628c67134b751563b09d32dd3aad5d738f6
Jesus Reyes <luisdejesus200122@gmail.com>
2024-05-01 22:59:49 -0600

    Feat: PDF Download Report implemented

--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -13,11 +13,13 @@ export const HomePage = () => {
     const [status, setstatus] = useState<Status>(Status.notStarted)
     const [hasInputError, setHasInputError] = useState<boolean>(false)
     const [onAddGasto, setOnAddGasto] = useState<boolean>(false)
+    const [onPdfDownload, setOnPdfDownload] = useState<boolean>(false)
     const {values,handleChange,resetForm} = useForm({
         nombre: '',
         total: '',
         fecha:''
     });
+    const {values: emailValue, handleChange: emailChange, resetForm: resetEmailInput}  = useForm({email: ''})
     const createGasto = async() => {
         if(values.nombre.length === 0 || values.total.length === 0 || values.fecha.length === 0)return setHasInputError(true);
         try {
@@ -26,19 +28,37 @@ export const HomePage = () => {
             CustomModals.showCustomModal('Gasto Creado Exitosamente', 'success');
             setstatus(Status.done);
             resetForm();
+            setHasInputError(false)
             await getGastos()
         } catch (error:any) {
             setstatus(Status.notStarted);
+            setHasInputError(false)
             CustomModals.showCustomModal('Ups Hubo un error', 'error', error.message);
             console.error(error);
             
         }
 
     }
+    const downloadPdf = async() => {
+      if(emailValue.email.length === 0) return setHasInputError(true);
+      try {
+        setstatus(Status.inProgress);
+        await axios.post('https://presupuesto-backend.onrender.com/api/presupuesto/pdf',{to:emailValue.email} );
+        setstatus(Status.done);
+        resetEmailInput();
+        setHasInputError(false)
+        CustomModals.showCustomModal('Reporte creado exitosamente', 'success', `El reporte se envio al correo: ${emailValue.email}`);
+      } catch (error: any) {
+        setstatus(Status.notStarted);
+        CustomModals.showCustomModal('Ups Hubo un error', 'error', error.message);
+        setHasInputError(false)
+        console.error(error);
+        
+      }
+    }
     const getGastos = async():Promise<GastosResponse> => {
             setstatus(Status.inProgress)
             const resp = await axios.get<GastosResponse>('https://presupuesto-backend.onrender.com/api/presupuesto/')
-            
             const data = resp.data;
             setgastos(data);
             setstatus(Status.done)
@@ -57,6 +77,7 @@ export const HomePage = () => {
     <div className="flex flex-col items-start m-4 p-2 justify-center">
         <h1 className="">Tabla de Gastos</h1>
         <PrimaryButton onClick={() => setOnAddGasto(!onAddGasto)} title={"Agregar Gasto"}/>
+        <PrimaryButton onClick={() => setOnPdfDownload(!onPdfDownload)} title={"Descargar PDF"}/>
         {
             onAddGasto && 
             <div className="w-[100%] mt-5">
@@ -64,6 +85,13 @@ export const HomePage = () => {
         <CustomTextfieldComponent value={values.total} errorMsg="El Monto total es obligatorio" error={values.total.length <=0 && hasInputError}  name="total" typeInput="number" title={"Ingresar Total Gastado"} onChange={handleChange}/>
         <CustomTextfieldComponent value={values.fecha} errorMsg="La Fecha es obligatorio" error={ values.fecha.length <=0  &&hasInputError} name="fecha" typeInput="datetime-local" title={"Ingresar Fecha"} onChange={handleChange}/>
         <PrimaryButton onClick={createGasto} disabled={status === Status.inProgress} title={"Crear Gasto"}/>
+        </div>
+        }
+        {
+            onPdfDownload && 
+            <div className="w-[100%] mt-5">
+        <CustomTextfieldComponent value={emailValue.email} errorMsg="El correo es Obligatorio" error={ emailValue.email.length <=0  &&hasInputError} name="email" typeInput="email" title={"Ingresa tu correo Electronico"} onChange={emailChange}/>
+        <PrimaryButton onClick={downloadPdf} disabled={status === Status.inProgress} title={"Enviar"}/>
         </div>
         }
         <CustomTable columns={colums} status={status}>
